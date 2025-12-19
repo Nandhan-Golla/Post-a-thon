637e2f7a9da520751e27553ea73df9afa92b4ff8
Nandhan Golla <nandhan.golla.work@gmail.com>
2025-12-19 22:40:25 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,13 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Post-A-Thon',
+  description: 'Created BY Nandhan',
+  generator: 'null',
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: 'null',
         media: '(prefers-color-scheme: light)',
       },
       {
